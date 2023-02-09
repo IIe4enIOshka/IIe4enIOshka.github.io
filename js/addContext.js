f3aed6032937fe2add28f96a94e370632cb72a29
Alexey Vyrodov <g47711@gmail.com>
2023-02-09 20:56:11 +0700

    new commit 2

--- js/addContext.js
+++
@@ -1,6 +0,0 @@
-var blah = document.body.querySelectorAll('.item');
-for (var x = 0; x < blah.length; x++) {
-	blah[x].addEventListener("contextmenu", function (event) {
-		event.preventDefault();
-	});
-}
--- js/gallery.js
+++
@@ -1,9 +0,0 @@
-function startGallery(){
-	baguetteBox.run('.gallery',{
-		captions: function(element) {
-			return element.getElementsByTagName('img')[0].alt;
-		},
-		buttons: 'auto',
-		animation: 'fadeIn',
-	});
-}
